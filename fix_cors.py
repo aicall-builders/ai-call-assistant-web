c54ba4df1a38e035a2620d44906cb739bb586181
정성민 <seongminj0613@gmail.com>
2026-06-15 15:08:34 +0900

    fix: keywords CORS 설정 스크립트

---
+++ fix_cors.py
@@ -0,0 +1,17 @@
+﻿import boto3
+client = boto3.client('apigateway', region_name='ap-northeast-2')
+
+client.put_integration_response(
+    restApiId='avrq2kzfp9',
+    resourceId='m28zi5',
+    httpMethod='OPTIONS',
+    statusCode='200',
+    responseParameters={
+        'method.response.header.Access-Control-Allow-Headers': "'Content-Type,Authorization'",
+        'method.response.header.Access-Control-Allow-Methods': "'GET,POST,DELETE,OPTIONS'",
+        'method.response.header.Access-Control-Allow-Origin': "'https://dk1k75g0ji3vw.cloudfront.net'",
+    }
+)
+
+client.create_deployment(restApiId='avrq2kzfp9', stageName='prod')
+print('완료!')
